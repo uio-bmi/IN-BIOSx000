01a4fcbd339b7b15027415452d9b2739fd455aa7
Torbjørn Rognes <torognes@ifi.uio.no>
2022-05-30 15:27:26 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'IN-BIOSx000'
 copyright = u'2018, Arvind Sundaram'
-author = u'Arvind Sundaram, Lex Nederbragt'
+author = u'Arvind Sundaram, Lex Nederbragt, Torbjørn Rognes'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -231,7 +231,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'IN-BIOSx000.tex', u'IN-BIOSx000 Documentation',
-   u'Arvind Sundaram, Lex Nederbragt', 'manual'),
+   u'Arvind Sundaram, Lex Nederbragt, Torbjørn Rognes', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
